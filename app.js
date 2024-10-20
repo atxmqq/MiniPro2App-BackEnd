287cfce185b9c567d012507337ddaaab28a3dc5c
atxmqq <atomgamer123@gmail.com>
2024-10-20 21:24:15 +0700

    ฟ

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ const userRouter =  require('./api/user');
 const riderRouter =  require('./api/rider');
 const productRouter =  require('./api/product');
 const cors = require('cors');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
-app.use("/user",userRouter);
-app.use("/rider",riderRouter);
-app.use("/product",productRouter);
+app.use("/user", userRouter);
+app.use("/rider", riderRouter);
+app.use("/product", productRouter);
+
+// เพิ่มการใช้ environment variable PORT
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
