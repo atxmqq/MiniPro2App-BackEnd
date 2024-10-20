176c719aa3dec95ad4346297ea0bbce4fb997074
atxmqq <atomgamer123@gmail.com>
2024-10-20 20:57:38 +0700

    port

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var app = express();
-const userRouter =  require('./api/user');
-const riderRouter =  require('./api/rider');
-const productRouter =  require('./api/product');
+const userRouter = require('./api/user');
+const riderRouter = require('./api/rider');
+const productRouter = require('./api/product');
 const cors = require('cors');
 var bodyParser = require('body-parser')
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
-app.use("/user",userRouter);
-app.use("/rider",riderRouter);
-app.use("/product",productRouter);
+app.use("/user", userRouter);
+app.use("/rider", riderRouter);
+app.use("/product", productRouter);
 
 module.exports = app;
